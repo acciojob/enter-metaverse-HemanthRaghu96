79bc390e25d2e5488dbecbc656fdbb9eeadb64db
Hemanth R <91587889+HemanthRaghu96@users.noreply.github.com>
2025-12-04 18:54:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-//your JS code here. If required.
 let statusId = document.getElementById("status");
 let enterBtn = document.getElementById("enterBtn");
 function handleClick() {
- statusId.innerHTML="<h1>Enter Metaverse</h1>"
+ statusId.innerHTML="<h1>Entered Metaverse</h1>"
 }
