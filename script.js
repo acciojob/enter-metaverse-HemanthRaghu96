cc3523d509d8f647cbab8cf809e1d6f7cc6cca8a
Hemanth R <91587889+HemanthRaghu96@users.noreply.github.com>
2025-12-04 18:50:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let statusId = document.getElementById("status");
+let enterBtn = document.getElementById("enterBtn");
+function handleClick() {
+ statusId.innerHTML="<h1>Enter the Metaverse</h1>"
+}
